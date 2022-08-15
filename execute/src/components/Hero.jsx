ffa25c6b3f320400d5a88ce327ceb8f75fb1fa1c
Kamoljonov S (Kori) <mr.sirojiddin4016810@gmail.com>
2022-08-15 13:44:20 +0900

    project finished

--- execute/src/components/Hero.jsx
+++ execute/src/components/Hero.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className="hero">
       <div className="content">
-      <p>Call us</p>
-      <p>1-800-123-4567</p>
-      <p>Because Money</p>
-      <p>Doesn't come with instructions</p>
-      <button href='/' className='button'>Free Consultation</button>
+        <p>Call us</p>
+        <p>1-800-123-4567</p>
+        <p>Because Money</p>
+        <p>Doesn't come with instructions</p>
+        <button href="/" className="button">
+          Free Consultation
+        </button>
+       
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
